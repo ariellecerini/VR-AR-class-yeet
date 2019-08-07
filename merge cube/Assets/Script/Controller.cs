64a6b921d17efa1fe622a3a0a380ef611f5ceb64
Arielle Cerini <arielle.cerini@createthenext.com>
2019-08-07 15:57:08 -0400

    wednesday

--- merge cube/Assets/Script/Controller.cs
+++ merge cube/Assets/Script/Controller.cs
@@ -7,6 +7,8 @@ public class Controller : MonoBehaviour
         
         public float speed = 1f;    //Variable for speed of pacman movement
         public GameObject spawn_1; 
+        public int dotsLeft; //variable for scoring parameters 
+        public GameObject startPoint; //used to return player to their original location 
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +19,25 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
-     {
-        transform.position += transform.forward * speed * Time.deltaTime;
-     }
+        Debug.Log("dotsLeft"); //this will print hoW MANY DOTS ARE LEFT IN OUR GAME 
+        ShowDots(); //run function ShowDots 
+
+     if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += transform.forward * -speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += transform.right * -speed * Time.deltaTime;
+        }
     }
     void OnTriggerEnter(Collider gate)
 {
@@ -30,5 +47,21 @@ if(gate.gameObject.tag == "Gate-1")
     transform.position = spawn_1.transform.position;
 }
 
+}
+
+void OnCollisionEnter(Collision collision)
+{
+    if(collision.gameObject.tag =="Dots"){
+        dotsLeft--;
+    } else if (collision.gameObject.tag == "Ghost"){
+        transform.position = startPoint.transform.position; 
+    }
+}
+
+void ShowDots()
+{
+    if(dotsLeft == 0){
+        Debug.Log("You Win!"); 
+    }
 }
 }
---
+++ merge cube/Assets/Script/Ghost_AI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost_AI : MonoBehaviour
+{
+	//List Variables 
+	public float ghostSpeed = 1f; //speed of the ghost 
+	public GameObject ghostStart; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
